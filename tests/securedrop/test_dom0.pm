e13dd9a1271cfba8a08d29e0e32096e31dd697f3
Rowen S <ro@freedom.press>
2025-08-22 11:20:14 -0400

    Force-update whonix before running tests; see https://github.com/freedomofpress/securedrop-workstation/issues/1333

--- tests/securedrop/test_dom0.pm
+++ tests/securedrop/test_dom0.pm
@@ -35,6 +35,10 @@ sub run {
     assert_script_run('rpm -q python3-pytest-cov || sudo qubes-dom0-update -y python3-pytest-cov', timeout => 300);
     script_run('ln -s /usr/share/securedrop-workstation-dom0-config/config.json /home/user/securedrop-workstation/config.json');
     script_run('ln -s /usr/share/securedrop-workstation-dom0-config/sd-journalist.sec /home/user/securedrop-workstation/sd-journalist.sec');
+
+    # Force-update whonix before running tests
+    # See https://github.com/freedomofpress/securedrop-workstation/issues/1333
+    script_run('sudo qubes-vm-update --show-output --targets whonix-gateway-17 --force-update');
     script_run("env CI=true make -C $sdw_path test | tee make-test.log", timeout => 2400);
 
 
